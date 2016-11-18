0c9935e01bca86a2e4abba99eb7db447a14d6a7b
Devon Weller <devon@tokenly.co>
2016-11-18 12:26:26 -0600

    Added addWithWrap method.
    + Use filepath references instead of content when adding files.
    + Added error handling.
    + Use new curl_file_create method for file uploading (Requires PHP>=5.5)

--- src/IPFS.php
+++ src/IPFS.php
@@ -6,6 +6,8 @@
 
 namespace Cloutier\PhpIpfsApi;
 
+use Exception;
+
 class IPFS {
 	private $gatewayIP;
 	private $gatewayPort;
@@ -24,14 +26,44 @@ class IPFS {
 
 	}
 
-	public function add ($content) {
+	/**
+	 * Adds a file directly to IPFS
+	 * @param string  $filepath a filepath to upload
+	 * @param boolean $pin      pin the file (true)
+	 * @return  string The content hash
+	 */
+	public function add ($filepath, $pin=true) {
 		$ip = $this->gatewayIP;
 		$port = $this->gatewayApiPort;
 
-		$req = $this->curl("http://$ip:$port/api/v0/add?stream-channels=true", $content);
-		$req = json_decode($req, TRUE);
+		$query_vars = [
+			'pin' => ($pin  ? 'true' : 'false'),
+		];
 
-		return $req['Hash'];
+		$response = $this->postFile("/api/v0/add", $filepath, $query_vars);
+		return $response['Hash'];
+	}
+
+	/**
+	 * Adds a file directly to IPFS
+	 * @param string  $filepath a filepath to upload
+	 * @param boolean $pin      pin the file (true)
+	 * @return  array Data with Name, Hash and FolderHash
+	 */
+	public function addWithWrap ($filepath, $pin=true) {
+		$ip = $this->gatewayIP;
+		$port = $this->gatewayApiPort;
+
+		$query_vars = [
+			'pin' => ($pin  ? 'true' : 'false'),
+			'w'   => 'true',
+		];
+
+		$response = $this->postFile("/api/v0/add", $filepath, $query_vars, $expect_multiple = true);
+
+		$return_data = $response[0];
+		$return_data['FolderHash'] = $response[1]['Hash'];
+		return $return_data;
 	}
 
 	public function ls ($hash) {
@@ -66,16 +98,48 @@ class IPFS {
 		return $data;
 	}
 
-    public function version () {
-        
-        $ip = $this->gatewayIP;
-        $port = $this->gatewayApiPort;
-        $response = $this->curl("http://$ip:$port/api/v0/version");
-        $data = json_decode($response, TRUE);
-        return $data["Version"];
-    }
+	public function version () {
+		
+		$ip = $this->gatewayIP;
+		$port = $this->gatewayApiPort;
+		$response = $this->curl("http://$ip:$port/api/v0/version");
+		$data = json_decode($response, TRUE);
+		return $data["Version"];
+	}
 
-	private function curl ($url, $data = "") {
+	public function get($api_path, $query_vars=[]) {
+		$ip = $this->gatewayIP;
+		$port = $this->gatewayApiPort;
+
+		$query_string = $query_vars ? '?'.http_build_query($query_vars) : '';
+		$response = $this->curl("http://{$ip}:{$port}/".ltrim($api_path,'/').$query_string);
+		$data = json_decode($response, true);
+		return $data;
+	}
+
+	public function postFile($api_path, $filepath, $query_vars=[], $expect_multiple=false) {
+		$ip = $this->gatewayIP;
+		$port = $this->gatewayApiPort;
+
+		$query_string = $query_vars ? '?'.http_build_query($query_vars) : '';
+		$response = $this->curl("http://{$ip}:{$port}/".ltrim($api_path,'/').$query_string, $filepath);
+		echo "\$response: $response\n";
+
+		if ($expect_multiple) {
+			$data = [];
+			foreach (explode("\n", $response) as $json_data_line) {
+				if (strlen(trim($json_data_line))) {
+					$data[] = json_decode($json_data_line, true);
+				}
+			}
+		} else {
+			$data = json_decode($response, true);
+		}
+
+		return $data;
+	}
+
+	private function curl ($url, $filepath=null) {
 		$ch = curl_init();
 
 		curl_setopt($ch, CURLOPT_URL, $url);
@@ -84,17 +148,38 @@ class IPFS {
 		curl_setopt($ch, CURLOPT_HEADER, 0);
 		curl_setopt($ch, CURLOPT_BINARYTRANSFER,1);
 		 
-		if ($data != "") {
-			curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: multipart/form-data; boundary=a831rwxi1a3gzaorw1w2z49dlsor')); 
+		if ($filepath !== null) {
+			// add the file
+			$cfile = curl_file_create($filepath, 'application/octet-stream', basename($filepath));
+
+			// post
+			$post_fields = ['file' => $cfile];
 			curl_setopt($ch, CURLOPT_POST, 1);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, "--a831rwxi1a3gzaorw1w2z49dlsor\r\nContent-Type: application/octet-stream\r\nContent-Disposition: file; \r\n\r\n" . $data . "    a831rwxi1a3gzaorw1w2z49dlsor");
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
 		}
 
 		$output = curl_exec($ch);
 
-		if ($output == FALSE) {
-			//todo: when ipfs doesn't answer
+		// check HTTP response code
+		$response_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		$code_category = substr($response_code, 0, 1);
+		if ($code_category == '5' OR $code_category == '4') {
+			$data = @json_decode($output, true);
+			if (!$data AND json_last_error() != JSON_ERROR_NONE) {
+				throw new Exception("IPFS returned response code $response_code: ".substr($output, 0, 200), $response_code);
+			}
+			if (is_array($data)) {
+				if (isset($data['Code']) AND isset($data['Message'])) {
+					throw new Exception("IPFS Error {$data['Code']}: {$data['Message']}", $response_code);
+				}
+			}
+		}
+
+		// handle empty response
+		if ($output === false) {
+			throw new Exception("IPFS Error: No Response", 1);
 		}		 
+
 		curl_close($ch);
  
 
